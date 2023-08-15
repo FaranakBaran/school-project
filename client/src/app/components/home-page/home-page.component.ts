f7632095c02bdfa2e4ef6dc49e0085745185c041
Faranak Baran <ffranakk1380@gmail.com>
2023-08-15 07:28:14 +0330

    morning change

--- client/src/app/components/home-page/home-page.component.ts
+++ client/src/app/components/home-page/home-page.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss']
+  styleUrls: ['./home-page.component.scss'],
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule]
 })
 export class HomePageComponent {
 
